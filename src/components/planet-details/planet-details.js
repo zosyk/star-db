5755f438c39dba1d29eb0b6b744fc6da70520f62
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-08-26 15:36:28 +0300

    created initial react components

---
+++ src/components/planet-details/planet-details.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './planet-details.css';
+
+export default class PlanetDetails extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1>Planets details</h1>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,52 +1,6 @@
-class SwapiService {
-    _apiBase = 'https://swapi.co/api';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-
-    async getAllPeople() {
-        const res = await this.getResource('/people/');
-        return res.results;
-    }
-
-    getPerson(id) {
-        return this.getResource(`/people/${id}`);
-    }
-
-    async getAllPlanets() {
-        const res = await this.getResource('/planets/');
-        return res.results;
-    }
-
-    getPlanet(id) {
-        return this.getResource(`/planets/${id}`);
-    }
-
-    async getAllStarships() {
-        const res = await this.getResource('/starships/');
-        return res.results;
-    }
-
-    getStarship(id) {
-        return this.getResource(`/starships/${id}`);
-    }
-
-    async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, received ${res.status}`)
-        }
-
-        const body = await res.json();
-        return body;
-    }
-}
-
-const swapi = new SwapiService();
-
-swapi.getPerson(1)
-    .then((p) => {
-        console.log(p);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+ReactDOM.render(<App />,
+    document.getElementById('root'));
